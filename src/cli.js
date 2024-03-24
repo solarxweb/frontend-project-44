6190cf4403bba79cc0b3d1fe957e041a98a6fc8a
dev <legendaryje@gmail.com>
2024-03-24 17:16:49 +0300

    brain-even logic added to cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,27 @@
 
 import readlineSync from 'readline-sync';
 
-const getNameGreetings = () => {
+export const getNameGreetings = () => {
   console.log('Welcome to Brain Games!');
   const getName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getName}!`);
 };
 
-export default getNameGreetings;
+export const getEvenNumber = () => {
+  const collOfNums = [15, 6, 7];
+  console.log('Welcome to Brain Games!');
+  const getName = readlineSync.question('May I have your name? ');
+  console.log('Answer "yes if the number is even, otherwise answer "no"');
+  for (let i = 0; i < collOfNums.length; i += 1) {
+    console.log(`Question: ${collOfNums[i]}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (collOfNums[i] % 2 === 0 && answer === 'yes') {
+      console.log('Correct!');
+    } else if (collOfNums[i] % 2 !== 0 && answer === 'no') {
+      console.log('Correct!');
+    } else {
+      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${getName}!`);
+    }
+  }
+  console.log(`Congratulations, ${getName}!`);
+};
