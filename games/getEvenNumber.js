6ca7deb197f68c94b2e5fec8349ee24d0b9e8fd1
dev <legendaryje@gmail.com>
2024-04-06 18:30:34 +0300

    small changes

--- games/getEvenNumber.js
+++ games/getEvenNumber.js
@@ -1,15 +1,16 @@
-import readlineSync from 'readline-sync';
-import { getNameGreetings, getInt } from '../src/index.js';
+import {
+  getNameGreetings, getInt, writeAnswer,
+  writeExercise, writeQuestion,
+} from '../src/index.js';
 
 const getEvenNumber = () => {
   const theName = getNameGreetings();
+  writeExercise('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const num = getInt();
-    console.log(`Question: ${num}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (num % 2 === 0 && answer === 'yes') {
-      console.log('Correct!');
-    } else if (num % 2 !== 0 && answer === 'no') {
+    writeQuestion(num);
+    const answer = writeAnswer();
+    if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
     } else {
       console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${theName}!`);
