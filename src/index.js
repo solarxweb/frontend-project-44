cb299ce1af4a72312a743cf255b148676a068c8d
dev <legendaryje@gmail.com>
2024-04-17 20:17:38 +0300

    misprint fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getNameGreetings = () => {
-  console.log('Welcome to Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const getName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getName}!`);
   return getName;
